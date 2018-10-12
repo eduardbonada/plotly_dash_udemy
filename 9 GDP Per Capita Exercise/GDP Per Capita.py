4fe60b7aa61cc77450ecdafe484538636ddd8ca5
eduardbonada <eduard.bonada@gmail.com>
2018-10-12 19:11:07 +0200

    GDP per capita exercise - add axis selectors

--- 9 GDP Per Capita Exercise/GDP Per Capita.py
+++ 9 GDP Per Capita Exercise/GDP Per Capita.py
@@ -1,4 +1,4 @@
-# Create a dashboard with a scatter plot showing GDP per capita vs life expectancy
+# Create a dashboard with a scatter plot showing configurable x and y axis
 
 import dash
 import dash_core_components as dcc
@@ -10,36 +10,64 @@ import pandas as pd
 # read data
 df = pd.read_csv('../data/gapminderDataFiveYear.csv')
 
+# set features to show in x/y axis
+features = ['pop','lifeExp','gdpPercap']
+
 # Launch the application
 app = dash.Dash()
 
 # Setup app layout
 app.layout = html.Div([
+
+  # dropdown to select xaxis
+  html.Div([
+    dcc.Dropdown(
+      id='xaxis',
+      options=[{'label': i.title(), 'value': i} for i in features],
+      value='gdpPercap'
+    )
+  ],
+  style={'width': '48%', 'display': 'inline-block'}),
+
+  # dropdown to select yaxis
+  html.Div([
+    dcc.Dropdown(
+      id='yaxis',
+      options=[{'label': i.title(), 'value': i} for i in features],
+      value='lifeExp'
+    )
+  ],
+  style={'width': '48%', 'display': 'inline-block'}),
+
   # scatterplot
-  dcc.Graph(
-    id='scatterplot'
-    # figure will be added by the decorated update_scatterplot_year function
-  ),
-  # dropdown
-  #dcc.Dropdown(
-  #  id = 'year-picker',
-  #  options = [{'label':str(y), 'value':y} for y in df['year'].unique()],
-  #  value = df['year'].max()
-  #),
-  #slider
-  dcc.Slider(
-    id = 'year-picker',
-    min = df['year'].min(),
-    max = df['year'].max(),
-    step = 5,
-    marks = {str(y): '{}'.format(y) for y in df['year'].unique()},
-    value=df['year'].max()
-  )
+  html.Div([
+    dcc.Graph(
+      id='scatterplot'
+      # figure will be added by the decorated update_scatterplot_year function
+    )
+  ]),
+
+  # slider for year-picker  
+  html.Div([
+    dcc.Slider(
+      id = 'year-picker',
+      min = df['year'].min(),
+      max = df['year'].max(),
+      step = 5,
+      marks = {str(y): '{}'.format(y) for y in df['year'].unique()},
+      value=df['year'].max()
+    )
+  ],
+  style={'width': '95%'})
+
 ])
 
 # Setup year_picker-scatterplot interaction through callback function
-@app.callback(Output('scatterplot', 'figure'), [Input('year-picker', 'value')])
-def update_scatterplot_year(year):
+@app.callback(
+  Output('scatterplot', 'figure'), 
+  [Input('year-picker', 'value'), Input('xaxis', 'value'), Input('yaxis', 'value')]
+)
+def update_scatterplot(year, xaxis, yaxis):
   # filter by year
   filtered_df = df[df['year'] == year]  
 
@@ -49,20 +77,26 @@ def update_scatterplot_year(year):
     df_by_continent = filtered_df[filtered_df['continent'] == continent]
     data.append(go.Scatter(
       name = continent,
-      x = df_by_continent['gdpPercap'],
-      y = df_by_continent['lifeExp'],
+      x = df_by_continent[xaxis],
+      y = df_by_continent[yaxis],
       text = df_by_continent['country'],
       mode = 'markers',
-      opacity = 0.7,
-      marker = {'size': 15},
+      marker = {
+        'size': 15, 
+        'opacity': 0.7, 
+        'line': {
+          'width': 0.5, 
+          'color': 'white'
+        }
+      },
     ))
 
   # return figure (data & layout)
   return {
     'data': data,
     'layout': go.Layout(
-      xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-      yaxis={'title': 'Life Expectancy'},
+      xaxis={'type': 'log', 'title': xaxis.title()},
+      yaxis={'title': yaxis.title()},
       hovermode='closest'
     )
   }
